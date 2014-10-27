058fbba928e4ef19307443ca28e858e43e010559
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-27 17:01:15 +0100

    Use tabs instead of spaces

--- test.js
+++ test.js
@@ -4,16 +4,16 @@ var base64Regex = require('./');
 var test = require('ava');
 
 test('match base64 encoded string', function (t) {
-    t.plan(3);
+	t.plan(3);
 
-    var fixtures = [
-        'dW5pY29ybg==',
-        'Ym9hdA=='
-    ];
+	var fixtures = [
+		'dW5pY29ybg==',
+		'Ym9hdA=='
+	];
 
-    fixtures.forEach(function (el) {
-        t.assert(base64Regex().test(el), el);
-    });
+	fixtures.forEach(function (el) {
+		t.assert(base64Regex().test(el), el);
+	});
 
-    t.assert(base64Regex().exec('asdasd dgggghey dW5pY29ybg== helllo')[0].trim() === 'dW5pY29ybg==');
+	t.assert(base64Regex().exec('asdasd dgggghey dW5pY29ybg== helllo')[0].trim() === 'dW5pY29ybg==');
 });
