2b32f32ab02e2f7b07197482cdb6d0af89e23027
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-29 16:45:17 +0100

    Tweaks

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * Regular expression for matching base64 encoded strings
- *
- * @param {Object} opts
- * @api public
- */
-
 module.exports = function (opts) {
 	opts = opts || {};
 	var regex = '(?:[A-Za-z0-9+\/]{4}\\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)';
