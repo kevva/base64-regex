733339f786a651c8246777241a2d77e8dc7767cd
Peter Müller <munter@fumle.dk>
2016-07-13 23:26:31 +0200

    Test matching an inline sourcemap syntax. Refs #1

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var test = require('ava');
 var base64Regex = require('./');
 
 test('match base64 encoded string', function (t) {
-	t.plan(3);
+	t.plan(4);
 
 	var fixtures = [
 		'dW5pY29ybg==',
@@ -15,4 +15,8 @@ test('match base64 encoded string', function (t) {
 	});
 
 	t.assert(base64Regex().exec('asdasd dgggghey dW5pY29ybg== helllo')[0] === 'dW5pY29ybg==');
+
+	// inline sourcemap
+	var sourceMapString = '//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImRlZmF1bHQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IktBQUssZ0JBQWdCLEFBQUMsQ0FBQyxFQUFDLENBQUcsTUFBSSxDQUFHLFVBQVMsVUFBUyxDQUFHLENBQUEsVUFBUyxDQUFHLENBQUEsU0FBUSxDQUFHO0FBQzFFLElBQUEsQ0FBQSxlQUFjLEVBQUksQ0FBQSxNQUFLLElBQUksQUFBQyxDQUFDLGtCQUFpQixDQUFDLGNBQWMsQUFBQyxDQUFDLFNBQVEsR0FBRyxDQUFHLEtBQUcsQ0FBRyxLQUFHLENBQUMsQ0FBQztBQUMxRixXQUZNLEFBQUM7VUFBSCxJQUFJLEFBQUMsQ0FBQyxhQUFZLENBQUMsQ0FBQztHQUVOLEFBQUMsRUFBQyxDQUFDO0FBQ3JCLE9BQU8sQ0FBQSxlQUFjLEFBQUMsRUFBQyxDQUFDO0NBQ3pCLENBQUMiLCJmaWxlIjoib3V0cHV0LmpzIn0=';
+	t.assert(base64Regex().test(sourceMapString), 'sourcemap no matched');
 });
