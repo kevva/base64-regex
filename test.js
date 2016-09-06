307066571f97d2ef05c086b3ff32edfed2eebc7d
Peter Müller <munter@fumle.dk>
2016-09-07 00:35:41 +0200

    Add more test fixtures
    Fixes #2.

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 import test from 'ava';
+import fixtures from './fixtures';
 import m from './';
 
-const fixtures = [
-	'dW5pY29ybg==',
-	'Ym9hdA=='
-];
-
 test('match base64 encoded string', t => {
+	const fixtures = [
+		'dW5pY29ybg==',
+		'Ym9hdA=='
+	];
+
 	for (const x of fixtures) {
 		t.true(m({exact: true}).test(x));
 	}
 
 	t.is(m().exec('asdasd dgggghey dW5pY29ybg== helllo')[0], 'dW5pY29ybg==');
+});
+
+test('match inline sourcemaps', t => {
+	for (const x of fixtures) {
+		t.true(m().test(x.split('data:application/json;base64,').pop()));
+	}
 });
