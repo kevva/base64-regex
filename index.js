21f718f4505d16d017f8a870a417132dc8686f80
Linus Unnebäck <linus@folkdatorn.se>
2020-10-06 11:11:18 +0200

    Tweak regex and add more tests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = options => {
 	options = Object.assign({}, options);
-	const regex = '(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)';
-	return options.exact ? new RegExp(`(?:^${regex}?$)`) : new RegExp(regex, 'g');
+	return options.exact ?
+		/^(?:[A-Za-z\d+/]{4})*(?:[A-Za-z\d+/]{2}==|[A-Za-z\d+/]{3}=)?$/ :
+		/\b(?:[A-Za-z\d+/]{4})*(?:[A-Za-z\d+/]{2}==|[A-Za-z\d+/]{3}=|[A-Za-z\d+/]{4}\b)/;
 };
